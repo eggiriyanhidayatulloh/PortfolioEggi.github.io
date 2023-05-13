fc7a877a5366df69054bfb664bc9981fa18d5f64
egi <egi@gmail.com>
2023-05-13 16:06:20 +0700

    Portfolio

---
+++ js/script.js
@@ -0,0 +1,97 @@
+// togglle icon navbar
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+// Scroll sections active kink
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    // Sticky navbar
+
+let header = document.querySelector('header');
+
+header.classList.toggle('sticky', window.scrollY > 100);
+
+// remove toggle icon and navbar when click navbar link
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+// scrollreveal
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom'});
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left'});
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right'});
+
+// typed js
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend', 'Youtuber', 'Flutter UI'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
+const typed2 = new Typed('.multiple-text2', {
+    strings: ['Frontend', 'Youtuber', 'Flutter UI'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
+//  submit contact
+
+const contactForm = document.getElementById("contact-form");
+const loader = document.querySelector(".loader");
+
+loader.style.display = "none";
+
+contactForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  loader.style.display = "block";
+  const url = e.target.action;
+  const formData = new FormData(contactForm);
+
+  fetch(url, {
+    method: "POST",
+    body: formData,
+    mode: "no-cors",
+  })
+    .then(() => {
+      loader.style.display = "none";
+      window.location.href = "/thankyou.html";
+    })
+    .catch((e) => alert("Error occured"));
+});
